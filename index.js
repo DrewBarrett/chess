554d0104ff50b380e9494452bc816d46810021c9
Drew Barrett <dfb1998@gmail.com>
2017-11-11 01:56:51 -0500

    uh oh spaghetti o's

--- index.js
+++
@@ -1,9 +0,0 @@
-var app = require('express')();
-
-app.get('/', function(req, res){
-    res.send('<h1>Hello worlds</h1>');
-});
-
-app.listen(3000, function(){
-    console.log('ayy server be up');
-});
